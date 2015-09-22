24270d831050d7fcd2a36c4d0222c527b49e896c
Yuncheng Li <raingomm@gmail.com>
2015-09-22 15:37:04 -0400

    compiled

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ mkdir _build
 CORENLP_JAR=/u/yli/workplace/web-apps/gif-caption-CF/3rdparty/stanford-corenlp-full-2015-04-20/stanford-corenlp-3.5.2.jar
 
 javac -d _build -cp "`pwd`/lib/*":$CORENLP_JAR `find javasrc -name '*.java'`
-cd _build && jar cf ../$jarfile .
+(cd _build && jar cf ../$jarfile .)
 ls -l $jarfile
 rm -rf _build
